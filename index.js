7c403320e655b3abb061685388014cc6b2574d07
joncancode <jc.haines19@gmail.com>
2017-11-19 15:12:47 -0500

    addition of mongoose and initial stages of an env file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get('/', function(req, res){
     res.send('basic skeleton for express app')
 })
 
 
-app.listen(3000, function(){
-    console.log('app is running on port 3000')
+app.listen(port, function(){
+    console.log('app is running on port ' + port)
 })
