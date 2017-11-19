3ab1f4945d1413e64592bcf7f6af65e2e05ecc89
joncancode <jc.haines19@gmail.com>
2017-11-19 17:04:09 -0500

    creation of models directory

---
+++ models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const todoScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Name required'
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
