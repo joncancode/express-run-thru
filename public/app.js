d91b7124f89cd897c590e10b92f3741a1f2fdd56
joncancode <jc.haines19@gmail.com>
2017-11-22 00:07:54 -0500

    addition of js to the form

--- public/app.js
+++ public/app.js
@@ -1,1 +1,38 @@
-alert('hello')
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos)
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo()
+        }
+    })
+});
+
+function addTodos(todos) {
+    //add todos to page
+    todos.forEach(function(todo){
+        addTodo(todo)
+    })
+}
+
+function addTodo(todo){
+    let newTodo = $(`<li class="task"> ${todo.name} ${todo.completed} </li>`)
+    if(todo.completed){
+        newTodo.addClass('done')
+    }
+    $('.list').append(newTodo)
+}
+
+function createTodo(){
+    //send req to create new todo
+    let userInput = $('#todoInput').val();
+    $.post('/api/todos',{name: userInput})
+    .then(function(newTodo){
+      $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  }
