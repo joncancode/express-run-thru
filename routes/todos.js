308f5306fdd1f8968694fd10977932a90c018773
joncancode <jc.haines19@gmail.com>
2017-11-21 20:49:45 -0500

    creation of helper file to consolidate code

--- routes/todos.js
+++ routes/todos.js
@@ -1,60 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const helpers = require('../helpers/todos');
 
-router.get('/', function(req, res) {
-  db.Todo
-    .find()
-    .then(function(todos) {
-      res.json(todos);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
+router
+  .route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo);
 
-router.post('/', function(req, res) {
-  db.Todo
-    .create(req.body)
-    .then(function(newTodo) {
-      res.status(201).json(newTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.get('/:todoId', function(req, res) {
-  db.Todo
-    .findById(req.params.todoId)
-    .then(function(foundTodo) {
-      res.json(foundTodo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.put('/:todoId', function(req, res) {
-  db.Todo
-    .findOneAndUpdate({ _id: req.params.todoId }, req.body)
-    .then(function(todo) {
-      res.json(todo);
-    })
-    .catch(function(err) {
-      res.send(err);
-    });
-});
-
-router.delete('/:todoId', function(req, res) {
-    db.Todo
-      .remove({ _id: req.params.todoId })
-      .then(function() {
-        res.json({message: 'deleted this'});
-      })
-      .catch(function(err) {
-        res.send(err);
-      });
-  });
+router.route('/:todoId')
+    .get(helpers.getOneTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
