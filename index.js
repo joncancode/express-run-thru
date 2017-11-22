9b908ba8c5c1dc2dad212c1141eaa0860b40c4c9
joncancode <jc.haines19@gmail.com>
2017-11-21 22:48:04 -0500

    imported jquery and added css styling

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const todoRoutes = require('./routes/todos')
 
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res){
-    res.send('basic skeleton for express app')
+    res.sendFile('index.html')
 })
 
 app.use('/api/todos', todoRoutes)
